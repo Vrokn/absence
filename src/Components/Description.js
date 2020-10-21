b5f13531eb37cedaa6e87bdf7827e4370434dc49
Vrokn <61293690+Vrokn@users.noreply.github.com>
2020-10-21 12:39:42 -0500

    Update content

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -7,63 +7,146 @@ export default function Description() {
 
     return (
         <Container className={'description'} id='about' fluid >
-            <h2>ABSENCE HALLOWEEN POOL PARTY</h2>            <br></br>
-            <Image src={storie} fluid />   <br></br> <br></br>          <p>¿QUÉ ES?<br></br>
-            Es el primer Pool Party que se llevará a cabo en Colombia en tiempos de pandemia de COVID-19 cumpliendo el 100% de los requerimientos legales actuales; Todos los asistentes deberán someterse a una prueba antes de asistir a fin de hacer el mejor esfuerzo posible para que la celebración de la cultura no tenga efectos negativos en la salud de los asistentes ni del resto de la población.
+
+                        <h2>ABSENCE HALLOWEEN POOL PARTY</h2>            <br></br>
+            <Image src={storie} fluid /> 
             <br></br>            <hr></hr>
-            ¿DÓNDE?<br></br>
-            Secret Location a 90 Km desde Bogotá, la locación se anunciará la mañana del 31 de octubre.
-            <br></br>            <hr></hr>
-            ¿CUÁNDO?<br></br>
-            31 OCT 2020 (3PM - END).
-            <br></br>            <hr></hr>
-            ¿Y LA PANDEMIA DE COVID-19?<br></br>
-            ¡Prueba de COVID-19 GRATUITA Y OBLIGATORIA + medidas de Bio-Seguridad!
-            <br></br>            <hr></hr>
-            ¿CÓMO SE HACEN LAS PRUEBAS DE COVID-19?<br></br>
-            El día antes del evento se pondrá a disposición de los asistentes una locación en Bogotá en donde una enfermera capacitada practicará las pruebas de COVID-19 de 3PM a 9PM. El resultado de la prueba tarda 10 minutos y el asistente podrá saber si tiene COVID-19 en ese momento o si lo tuvo en el pasado.
-            <br></br>            <hr></hr>
-            ¿QUÉ TIPO DE PRUEBA DE COVID SE PRÁCTICA Y CUÁL ES SU CONFIABILIDAD?<br></br>
-            Las pruebas utilizadas son las de SD BIOSENSOR que se utilizan en el Aeropuerto el Dorado en Bogotá. Tinen registro sanitario del INVIMA y validación secundaria y verificación de desempeño de prueba rápida "COVID-19 IgG/IgM Duo del Instituto Nacional de Salud". La documentación asociada puede ser solicitada en la línea de atención del evento.
-            <br></br>            <hr></hr>
-            ¿QUÉ PASA SI EL RESULTADO DE MI PRUEBA DE COVID-19 ES POSITIVO?<br></br>
-            Lamentablemente, no podrás acompañarnos al evento, pero podrás ceder tu entrada a otra persona si ella paga el valor de la prueba (85K) y obtiene un resultado negativo.
-            <br></br>            <hr></hr>
-            ¿POR QUÉ LAS PRUEBAS SE PRACTICAN EL DÍA ANTERIOR?<br></br>
-            Para que cualquier persona que tenga un resultado positivo de COVID-19 tenga tiempo de ceder su entrada a otro asistente si lo desea y por razones de logística.
-            <br></br>            <hr></hr>
-            ¿QUÉ PASA SI MI PRUEBA ES EL RESULTADO DE LA PRUEBA ES NEGATIVO PERO ME CONTAGIO DE COVID-19 EN LAS HORAS TRANSCURRIDAS DESDE LA TOMA DE LA MUESTRA Y EL INICIO DEL EVENTO?
-            <br></br>Según la OMS, hay una ventana de por lo menos dos días entre el momento del contagio y el tiempo de incubación necesario para volverse transmisor, esto significa que quien obtenga un resultado negativo en la prueba, no podría contagiar a nadie por lo menos durante los dos días siguientes a la toma de la prueba.
-            <br></br>            <hr></hr>
-            ¿QUÉ INCLUYE Y CUÁNTO VALE?<br></br>
-            Primera preventa 199K: Incluye transporte ida y vuelta desde Bogotá y Prueba de COVID-19 GRATUITA Y OBLIGATORIA para todos los asistentes.
-            <br></br>            <hr></hr>
-            ¿FORMAS DE PAGO?<br></br>
-            El pago se puede hacer con dinero en efectivo o tarjeta de crédito o débito en nuestras oficinas. También disponemos de compra online (tarjeta de crédito, botón PSE, baloto y/o efecty). Verificar disponibilidad y solicitar links de pago via WhatsApp en el 317 513-2529.
-            <br></br>            <hr></hr>
-            ¿FACILIDADES DE PAGO?<br></br>
-            Puedes reservar tu cupo de preventa con 100K y el resto del dinero deber ser pagado dos semanas antes de la realización del evento. Verificar disponibilidad y solicitar links de pago via WhatsApp en el 317 513-2529.
-            <br></br>            <hr></hr>
-            ¿PALCOS Y HABITACIONES?<br></br>
-            Tendremos disponibles un número muy limitado de palcos y habitaciones. Verificar disponibilidad y solicitar links de pago via WhatsApp en el 317 513-2529.
-            <br></br>            <hr></hr>
-            ¿LINE UP?<br></br>
-            &sdot; ✔️ HEADLINER TBA            <br></br>
-            &sdot; ✔️ SEBASTIÁN ORTÍZ (Baum)            <br></br>
-            &sdot; ✔️ STAV (Baum)            <br></br>
-            &sdot; ✔️ VALERIO (Octava)            <br></br>       
-            &sdot; Aleja Vargas (Deejay Academy)            <br></br>
-            &sdot; Cast Lov (Clap Team)            <br></br>
-            &sdot; Harvey Tie (Absence)<br></br>
-            &sdot; Gabriel Forero (Aftermath)<br></br>
-            &sdot; José Labeat (Clap Team)<br></br>
-            &sdot; Julián D'Koch (Awake)<br></br>
-            &sdot; Luff (Absence)<br></br>
-            &sdot; Mendoza (Massive)<br></br>
-            &sdot; Vrokn (Absence)<br></br>
-            &sdot; + New Talent Contest<br></br>
-                <br></br><br></br><hr></hr>
-            *PULEP XGK414</p><br></br><br></br><hr></hr>
+
+¿QUÉ ES?<br></br> 
+Es el primer evento privado que se llevará a cabo en Colombia en tiempos de pandemia de COVID-19 cumpliendo el 100% de los requerimientos legales actuales (en especial los del Decreto 1746 de 2020 de 1 de octubre de 2020 Expedido por el Ministerio de Salud y de Protección Social por medio del cual se adopta el protocolo de Bioseguridad para mitigar y controlar el riesgo del Coronarvirus COVID-19 en la realización de actividades culturales). 
+<br></br>            <hr></hr>
+
+
+¿Y LA PANDEMIA DE COVID-19?<br></br>
+¡Prueba de COVID-19 GRATUITA Y OBLIGATORIA + medidas de Bio-Seguridad!
+<br></br> <br></br> 
+
+
+
+Todos los asistentes (sin excepción) deberán someterse a una prueba de COVID-19 Y OBTENER UN RESULTADO NEGATIVO el día anterior al evento a fin de hacer el mejor esfuerzo humanamente posible para que no tengamos ningún efecto negativo en la salud de los asistentes, de sus allegados y del resto de la población. 
+<br></br>            <hr></hr>
+
+
+¿CÓMO SE HACEN LAS PRUEBAS DE COVID-19?<br></br>
+El día antes del evento se pondrá a disposición de los asistentes una locación en Bogotá en donde una enfermera capacitada practicará las pruebas de COVID-19 de 3PM a 9PM. El resultado de la prueba tarda 10 minutos y el asistente podrá saber si tiene COVID-19 en ese momento o si lo tuvo en el pasado.
+<br></br>            <hr></hr>
+
+
+¿QUÉ TIPO DE PRUEBA DE COVID SE PRÁCTICA Y CUÁL ES SU CONFIABILIDAD?<br></br>
+Las pruebas utilizadas son las de SD BIOSENSOR que se utilizan en el Aeropuerto el Dorado en Bogotá. Tinen registro sanitario del INVIMA y validación secundaria y verificación de desempleño de prueba rápida "COVID-19 IgG/IgM Duo del Instituto Nacional de Salud". La documentación asociada puede ser solicitada en cualquier momento.
+<br></br>            <hr></hr>
+
+
+¿QUÉ PASA SI EL RESULTADO DE MI PRUEBA DE COVID-19 ES POSITIVO?<br></br>
+Lamentablemente, no podrás acompañarnos a nuestro evento privado, pero podrás ceder tu derecho de ingreso a otra persona si ella paga el valor de la prueba (85K) y obtiene un resultado negativo.
+<br></br>            <hr></hr>
+
+
+¿POR QUÉ LAS PRUEBAS SE PRACTICAN EL DÍA ANTERIOR?<br></br>
+Para que cualquier persona que tenga un resultado positivo de COVID-19 tenga tiempo de ceder su derecho de ingreso a otro asistente que cumpla con el perfil solicitadoe si lo desea y por razones de logística.
+<br></br>            <hr></hr>
+
+
+¿QUÉ PASA SI EL RESULTADO DE LA PRUEBA ES NEGATIVO PERO ME CONTAGIO DE COVID-19 EN LAS HORAS TRANSCURRIDAS DESDE LA TOMA DE LA MUESTRA Y EL INICIO DEL EVENTO?<br></br>
+Según la OMS, hay una ventana de por lo menos dos días entre el momento del contagio y el tiempo de incubación necesario para volverse transmisor, esto significa que quien obtenga un resultado negativo en la prueba, no podría contagiar a nadie por lo menos durante los dos días siguientes a la toma de la prueba.
+<br></br>            <hr></hr>
+
+
+¿QUÉ INCLUYE Y CUÁNTO VALE?<br></br>
+Primera preventa 199K: Incluye transporte ida y vuelta desde Bogotá y Prueba de COVID-19 GRATUITA Y OBLIGATORIA para todos los asistentes.
+<br></br>            <hr></hr>
+
+
+¿DÓNDE?<br></br>
+Secret Location a 90 Km desde Bogotá, la locación se anunciará la mañana del 31 de octubre.
+<br></br>            <hr></hr>
+
+
+¿CUÁNDO?<br></br> 
+31 OCT 2020 (3PM - END).
+<br></br>            <hr></hr>
+
+
+¿FORMAS DE PAGO?<br></br>
+El pago se puede hacer con dinero en efectivo o tarjeta de crédito o débito en nuestras oficinas. También disponimos de compra online (tarjeta de crédito, botón PSE, baloto y/o efecty). Verificar disponibilidad y solicitar links de pago via WhatsApp en el 317 513-2529.
+<br></br>           
+
+<br></br> 
+
+El pago Online genera une recargo de 8% por concepto de comisiones bancarias, esta comisión no aplica para transferencias a nuestras cuentas de Bancolombia y/o Davivienda. 
+<br></br>            <hr></hr>
+
+
+¿FACILIDADES DE PAGO?<br></br>
+Puedes reservar tu cupo de preventa con 100K y el resto del dinero deber ser pagado una semana antes de la realización del evento. El dinero de la reserva no es reembolsable si no se completa el pago. Verificar disponibilidad y solicitar links de pago via WhatsApp en el 317 513-2529.
+<br></br>            <hr></hr>
+
+
+¿CUÁL ES EL AFORO DEL EVENTO? <br></br>
+El aforo está estrictamente limitado a 50 personas dando cumplimiento a lo dispuesto en el Decreto 1746 de 2020 de 1 de octubre de 2020 Expedido por el Ministerio de Salud y de Protección Social y en la La Resolución 1462 del Ministerio de Salud y Protección. Por otra parte contamos con un espacio al aire libre donde podemos garantizar que en el desarrollo del evento privado no habrá aglomeraciones de acuerdo a la definición legal de ese concepto, la cual acatamos a cabalidad y de la manera más estricta. 
+<br></br>            <hr></hr>
+¿EL EVENTO ESTÁ ASEGURADO? <br></br>
+Sí. Contamos con una póliza de seguro de responsabilidad civil contractual y extracontractual expedida por Seguros del Estado. 
+<br></br><hr></hr>
+¿QUÉ PASA SI EL EVENTO NO PUEDE LLEVARSE A CABO POR ALGUNA DECISIÓN DEL GOBIERNO NACIONAL O LOCAL?<br></br>
+Debido a que la naturaleza jurídica de nuestro evento es la de un evento privado y a que realmente cumplimos con todos los protocolos de Bio-Seguridad del Decreto 1746 de 2020 de 1 de octubre de 2020 Expedido por el Ministerio de Salud y de Protección Social, nuestro evento se puede llevar a cabo de acuerdo a las normas vigentes, sin embargo, si se llegara a expedir otra norma que impida la realización, el evento quedará aplazado para la fecha más cercana posible y si no fuera posible realizarlo en fechas cercanas, se procederá a realizar la devolución del dinero en los términos del Decreto 818 de 4 de junio de 2020.
+<br></br><hr></hr>
+¿PALCOS Y HABITACIONES?<br></br>
+Tendremos disponibles un número muy limitado de palcos y habitaciones. Verificar disponibilidad y solicitar links de pago via WhatsApp en el 317 513-2529.
+<br></br>            <hr></hr>
+
+
+HABITACIÓN VIP:
+<br></br>            <hr></hr>
+
+
+✅ Capacidad: 8 personas (2 por cama) en dos camas dobles y dos camas semi-dobles. <br></br>
+
+
+✅ Incluye: Baño privado | Ventilador | Closet | Llave para dejar cerrado | Servicio de seguridad fuera de la habitación.<br></br>
+
+
+✅ Precio: $1'000.000 por la habitación completa o $300.000 por pareja (una cama).<br></br>
+
+<br></br>           
+
+PALCOS:
+
+<br></br>            <hr></hr>
+
+✅ Incluye: Botella de Wiskhy o de Ginebra a elección + 6 Redbull, Agua o Gatorade | Baños privados para palcos. <br></br>
+
+
+✅ Capacidad: 6 personas.<br></br>
+
+
+✅ Precio: $1'000.000.<br></br>
+<br></br>            
+
+
+¿LINE UP?            <br></br>            <hr></hr>
+
+✔️ HERNÁN CAYETANO (Baum) <br></br>
+✔️ SEBASTIÁN ORTÍZ (Baum)<br></br>
+✔️ STAV (Baum) <br></br>
+✔️ VALERIO (Octava) <br></br>
+<br></br>            
+
+
+&sdot; Aleja Vargas (Deejay Academy)<br></br>
+&sdot; Daniel Yate (Absence) <br></br>
+&sdot; Harvey Tie (Absence)<br></br>
+&sdot; Gabriel Forero (Aftermath)<br></br>
+&sdot; Johanna (Absence) <br></br>
+&sdot; Julián D'Koch (Awake) <br></br>
+&sdot; Luff (Absence) <br></br>
+&sdot; Mendoza (Massive) 
+&sdot; Vrokn (Absence)<br></br>
++ New Talent Contest by Absence<br></br>
+
+<br></br>
+*Términos y condiciones www.absence.com.co
+<br></br>
         </Container>
     )
 }
